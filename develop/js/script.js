96d4a5bdf011ca6d518c86a35828d93940c391a0
katsaymeow <107221758+katsaymeow@users.noreply.github.com>
2022-08-23 16:17:24 -0400

    wrote in innerHTML

--- develop/js/script.js
+++ develop/js/script.js
@@ -10,8 +10,6 @@ var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=45103&APPI
      })
      .then(function (data){
         console.log(data);//this logs the data array to pull 
-      //   for ( var i =0; i < data; i++){
-      //       var day = data[i];
             var temp = data.main.temp;
             console.log(temp);
             var highs = data.main.temp_max;
@@ -28,13 +26,24 @@ var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=45103&APPI
             console.log(windSpeed);
             var windDegree = data.wind.deg;
             console.log(windDegree);
-            console.log(data);
-        
      });
      
      var userInput = document.querySelector('#submit');
-   //   userInput.addEventListener('click' );
-console.log(userInput)
+console.log(userInput);// this logs to the correct input
+document.getElementById("city").innerHTML = city;
+document.getElementById("temp").innerHTML = temp;
+document.getElementById("highs").innerHTML = highs;
+document.getElementById("lows").innerHTML = lows;
+document.getElementById("sun-sys").innerHTML = sunSys;
+document.getElementById("moon-sys").innerHTML = moonSys;
+document.getElementById("wind-speed").innerHTML = windSpeed;
+document.getElementById("wind-degree").innerHTML = windDegree;
+
+
+
+
+
+
 
 
 
